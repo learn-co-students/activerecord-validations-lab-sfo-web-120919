4fa7f214def71ccc6e32c84ead1ba963be825810
coronaisme <coronaisme@gmail.com>
2020-01-14 12:22:31 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+ 
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction ) }
+  validate :clickbait
+  
+  BAD = [
+    /Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/
+  ]
+
+
+  def clickbait
+    if BAD.none? { |element| element.match title}
+      errors.add(:title, "Bad Title")
+    end
+  end
+
 end
