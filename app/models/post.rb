e8841b6747105360e297d2d8f59fb31ed6578921
Donny <edl1155@gmail.com>
2020-01-14 13:48:05 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+
+# All posts have a title
+  validates :title, presence: true
+# Post content is at least 250 characters long
+  validates :content, length: { minimum: 250}
+# Post summary is a maximum of 250 characters
+  validates :summary, length: { maximum: 250}
+# Post category is either Fiction or Non-Fiction 
+# This step requires an inclusion validator, 
+# which was not outlined in the README lesson. 
+# You'll need to refer to the Rails guide to 
+# look up how to use it.
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait
+  
+  def clickbait
+    if !title.nil? && (!title.include?("You Won't" || "Secret" || "Top" || "Guess")) 
+      errors.add(:clickbait, "That is not a clickbait worthy title. Try again.")
+    end
+  end
+
+
 end
