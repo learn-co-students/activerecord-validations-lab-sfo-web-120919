59dceeb477de97f90fc1173766d564114c6f33e7
github username <ingrey1@gmail.com>
2020-01-14 10:27:34 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates_with TitleValidator
+    belongs_to :author
+    validates :title, presence: true
+    validates :content, length: {minimum: 15}
+    validates :summary, length: {maximum: 50}
+    validates :category, acceptance: {accept: ['Fiction']}
 end
--- db/migrate/20151202061742_create_posts.rb
+++ db/migrate/20151202061742_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration
       t.text :content
       t.text :summary
       t.string :category
-
+      t.belongs_to :author
       t.timestamps null: false
     end
   end
